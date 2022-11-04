2a311845ca17b014de79eeceb122c9700b57a547
I Y <waooncat@gmail.com>
2022-11-05 00:07:52 +0900

    ユーザー登録　ルーティング

--- AtlasSNS/app/Http/Controllers/PostsController.php
+++ AtlasSNS/app/Http/Controllers/PostsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+
+    public function register(){
+        return view('posts.register');
+    }
     //
     public function index(){
         return view('posts.index');
--- AtlasSNS/resources/views/auth/register.blade.php
+++ AtlasSNS/resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-{!! Form::open() !!}
+{!! Form::open(['url'=>'post/register']) !!}
 
 <h2>新規ユーザー登録</h2>
 
@@ -19,6 +19,8 @@
 {{ Form::text('password-confirm',null,['class' => 'input']) }}
 
 {{ Form::submit('登録') }}
+
+
 
 <p><a href="/login">ログイン画面へ戻る</a></p>
 
